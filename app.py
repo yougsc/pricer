d87b8bd1662cc3d89ab3a5ba518e4a8e2e8e2c4a
yougsc <charlie.huyghe@gmail.com>
2026-09-12 12:13:24 +0100

    added more functionalities to the streamlit interface

--- app.py
+++ app.py
@@ -2,7 +2,14 @@ from src.monte_carlo import monte_carlo, monte_carlo_anti
 from src.black_scholes import call, put, greeks_op, greeks_val
 import streamlit as st
 import matplotlib.pyplot as plt
-
+if "all_res_c" not in st.session_state:
+    st.session_state.all_res_c = []
+if "all_res_p" not in st.session_state:
+    st.session_state.all_res_p = []
+if "prev_plot" not in st.session_state:
+    st.session_state.prev_plot = None
+if "x_axis" not in st.session_state:
+    st.session_state.x_axis = "S"
 if __name__ == "__main__":
     st.header("Option Pricer")
     st.subheader("Analytical Method")
@@ -16,41 +23,70 @@ if __name__ == "__main__":
     col1, col2, col3, col4, col5, col6 = st.columns(6)
     with col1:
         S = st.number_input("Spot (S)", value=100.0)
+
     with col2:
         K = st.number_input("Strike (K)", value=100.0)
+
     with col3:
         T = st.number_input("Maturity (T)", value=1.0)
+
     with col4:
         r = st.number_input("Risk-free rate (r)", value=0.05)
+
     with col5:
         q = st.number_input("Dividend yield", value=0.0)
+
     with col6:
         sigma = st.number_input("Volatility (σ)", value=0.20)
-
+    x_axis_choice = st.radio(
+    "X-axis variable",
+    options=["S", "K", "T", "r", "q", "sigma"],
+    horizontal=True,
+)
+    st.session_state.x_axis = x_axis_choice
     show_call = st.toggle("Call")
     show_put = st.toggle("Put")
 
     if st.button("Add"):
         st.session_state.latest_result = {
-            "S": S, "K": K, "T": T, "r": r, "q": q, "sigma": sigma
+            "S": S, "K": K, "T": T, "r": r, "q": q, "sigma": sigma,"call": show_call, "put": show_put
         }
-
+    if st.button("Clear"):
+        st.session_state.all_res_c=[]
+        st.session_state.all_res_p=[]
+        st.session_state.prev_plot=None
+        st.session_state.latest_result = None
+        
     if st.session_state.latest_result is not None:
-        p = st.session_state.latest_result
+        if st.session_state.latest_result!=st.session_state.prev_plot:
+            p = st.session_state.latest_result
+            
+
+            if show_call:
+                resultc = call(p["S"], p["K"], p["T"], p["r"], p["q"], p["sigma"])
+                st.session_state.all_res_c.append({"result":resultc,"S":p["S"],"K": p["K"],"T": p["T"],"r": p["r"],"q": p["q"],"sigma": p["sigma"]})
+                
+
+            if show_put:
+                resultp = put(p["S"], p["K"], p["T"], p["r"], p["q"], p["sigma"])
+                st.session_state.all_res_p.append({"result":resultp,"S":p["S"],"K": p["K"],"T": p["T"],"r": p["r"],"q": p["q"],"sigma": p["sigma"]})
+                
+            st.session_state.prev_plot=p 
         fig, ax = plt.subplots()
-
-        if show_call:
-            resultc = call(p["S"], p["K"], p["T"], p["r"], p["q"], p["sigma"])
-            ax.scatter(p["S"], resultc, c="b", label="Call")
-
-        if show_put:
-            resultp = put(p["S"], p["K"], p["T"], p["r"], p["q"], p["sigma"])
-            ax.scatter(p["S"], resultp, c="r", label="Put")
-
-        ax.set_xlabel("Spot Price")
+        if show_call :
+            ax.scatter([d[st.session_state.x_axis] for d in st.session_state.all_res_c],
+                       [d["result"] for d in st.session_state.all_res_c],
+                       c="b", label="Call")
+        if show_put :
+            ax.scatter([d[st.session_state.x_axis] for d in st.session_state.all_res_p],
+                       [d["result"] for d in st.session_state.all_res_p],
+                       c="r", label="Put")
+        ax.set_xlabel(st.session_state.x_axis)
         ax.set_ylabel("Option Price")
         if show_call or show_put:
             ax.legend()
+            
+            
         st.pyplot(fig)
         N= st.slider(label="Number of Paths for Monte Carlo estimation", min_value=1000,max_value=3000000, value =300000)
         anti = st.toggle("Antithetic variates")
